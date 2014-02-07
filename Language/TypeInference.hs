41e03efd912a40ee10d3934b60029037ad2f19ea
fermat <fermat1217@gmail.com>
2014-02-07 10:16:40 -0600

    fixed typeinference

--- Language/TypeInference.hs
+++ Language/TypeInference.hs
@@ -1,12 +1,15 @@
-module Language.TypeInference where
-import qualified Data.Map as M
-import qualified Data.Set as S
+module Language.TypeInference
+       (runSolve, multiSub, isSolvable, Constraints)where
 import Language.Syntax
+
 import Control.Applicative hiding (empty)
 import Control.Monad.State.Lazy
 import Control.Monad.Identity
 import Control.Monad.Reader
+
 import Data.Char
+import qualified Data.Map as M
+import qualified Data.Set as S
 
 type Constraints = [(EType, EType)]
 
@@ -19,7 +22,7 @@ infer (PVar x) = do
   case lookup x m of
     Just a -> return (a, [])
     Nothing -> 
-      if (isUpper $ head x) then do
+      if not (isLower $ head x) then do
         n <- get
         modify (+1)
         lift $ modify (\ y -> (x, (EVar $ "X" ++ show n)): y)
@@ -70,91 +73,67 @@ infer (Imply p1 p2) = do
   (a2, c2) <- infer p2 
   return (Form, (a2, Form):(a1, Form):(c1 ++ c2)) 
 
-test1 = 
-  let s = runIdentity $ runStateT (runStateT (infer $ TApp (SApp (PVar "Vec") (PVar "U")) (PVar "n")) 0) []
-      (t,c) = (fst . fst) s
-      def = snd s in
-  putStrLn $ show def
-
+-- test1 = 
+--   let s = runIdentity $ runStateT (runStateT (infer $ TApp (SApp (PVar "Vec") (PVar "U")) (PVar "n")) 0) []
+--       (t,c) = (fst . fst) s
+--       def = snd s in
+--   putStrLn $ show def
 
 -- test2 = do
 --  s <- runStateT (runStateT (infer $ PSApp (PVar "Vec") (PMApp (PVar "U") (PVar "n"))) 0) []
 --  putStrLn $ show s
 
--- single step solving, the order matters!
+-- Implementing a Constraints solving algorithm
+-- from Prof. Stump's book.
+-- single step solving, the order of each case matters!
 step :: Constraints -> Constraints
 step ((EVar x,a):l) =
-      if x `S.member` (vars a) then ((EVar x,a):l)
-      else if x `S.member` (lVars l) then
-             (EVar x,a):(map (\ z -> (sub a x (fst z),sub a x (snd z))) l)
-           else ((EVar x,a):l)
+  if x `S.member` (vars a) then (EVar x,a):l
+  else if x `S.member` (lVars l)
+       then (EVar x,a): map helper l
+       else (EVar x,a):l
+    where helper (z1,z2) = (sub a x z1, sub a x z2)
 
-step ((a1, EVar x):l) =
-  case a1 of
-    EVar y ->
-      (a1, EVar x):l
-    _ -> if x `S.member` (vars a1) then
-           (a1, EVar x):l
-         else (EVar x, a1):l
+step ((a1, EVar x):l) = case a1 of
+                          EVar y -> (a1, EVar x):l
+                          _ -> if x `S.member` (vars a1) then (a1, EVar x):l
+                               else (EVar x, a1):l
 
-step ((To a1 a2, To b1 b2):l) =
-  (a1, b1):(a2,b2):l
+step ((To a1 a2, To b1 b2):l) = (a1, b1):(a2,b2):l
 
-step ((a1, a2):l) =  if a1 == a2 then l
-                      else (a1, a2):l
+step ((a1, a2):l) =  if a1 == a2 then l else (a1, a2):l
 
 lVars :: Constraints -> S.Set VName
-lVars [] = S.empty
-lVars ((a1,a2):l) =
-  S.union (S.union (vars a1) (vars a2)) (lVars l)
-
+lVars l = foldr (\ x z -> helper x `S.union` z) S.empty l
+  where helper (a1, a2) = vars a1 `S.union` vars a2
+        
 -- man, this is so cool  
 solve :: Constraints -> Int -> Constraints
 solve l n | n == length l = l
           |  n /= length l =
-            let l0 = step l
-            in
-             if l == l0 then
-               let l1 = (tail l ++ [head l])
-               in solve l1 (n+1)
-             else solve l0 0
+            let l0 = step l in
+            if l == l0 then solve (tail l ++ [head l]) (n+1)
+            else solve l0 0
 
-isSolvable :: Constraints -> Int -> Bool
- -- for a relax notion of solvable, use:  x `S.member` (S.union (lVars l) (vars t))
-isSolvable [] _ = True
-isSolvable ((EVar x, t):l) n | n /= length l +1 = if S.null $ vars t 
-                                                  then isSolvable (l++[(EVar x, t)]) (n+1)
-                                                  else False
-                             | n == length l +1 = True
-isSolvable _ _ = False
--- example by Prof. Stump's
+runSolve l = solve l 0
+
+isSolvable :: Constraints -> Bool
+isSolvable l =  null [i | i <- ensure l, i == False]
+  where ensure l = map (\ x -> pat x) l
+        pat (EVar x, t) = S.null $ vars t
+        pat _ = False
+        
 multiSub :: Constraints -> EType -> EType
 multiSub c (EVar x) =
-  let c1 = map (\ ((EVar y), t) -> (y, t)) c
-  in
-   case lookup x c1 of
-     Nothing -> EVar x
-     Just b -> b
+  case lookup (EVar x) c of
+    Nothing -> EVar x
+    Just b -> b
+    
 multiSub c Form = Form
 multiSub c Ind = Ind
 multiSub c (To a b) = To (multiSub c a) (multiSub c b)
 
-test3 = solve
-        [(To (To (EVar "Y") (EVar "Z")) (EVar "W"),To (EVar "X") (EVar "X")),(EVar "W",To (EVar "A") (EVar "A"))] 0
-{-
-check :: Constraints -> Int -> Bool
-check ((EVar x, t):l) n | n /= (length l)+1 
-                           = if x `S.member` vars t
-                             then False
-                             else if x `S.member` lVars l
-                                  then False
-                                  else check (l ++ [(EVar x,t)]) (n+1)
-                        | n == (length l)+1 = True
-                                              
-unify :: PreMeta -> EType -> InfCxt Bool
-unify m t = do
-  (a, c) <- infer m
-  let c1 = solve ((a,t):c) 0 in
-    return $ check c1 0
--}
+-- example by Prof. Stump's
+-- test3 = solve
+--         [(To (To (EVar "Y") (EVar "Z")) (EVar "W"),To (EVar "X") (EVar "X")),(EVar "W",To (EVar "A") (EVar "A"))] 0
 
