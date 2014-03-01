96878a53c0646140270dda1aa52beb6645c9e89f
fermat <fermat1217@gmail.com>
2014-03-01 11:47:32 -0600

    unify the namespace

--- Language/Syntax.hs
+++ Language/Syntax.hs
@@ -1,9 +1,9 @@
 module Language.Syntax
        (VName, EType(..), vars, sub,
-        PreTerm(..), Proof(..), ProofScripts,
+        PreTerm(..), ProofScripts,
         Prog(..), Args(..), FType(..),
         Datatype(..), Module(..), Decl(..),
-        fv, fVar, fPrVar, runSubst, runSubPre, runSubProof, naiveSub) where
+        fv, runSubst, naiveSub) where
 
 import Control.Monad.State.Lazy
 import Control.Monad.Reader
@@ -40,8 +40,21 @@ data PreTerm = PVar VName
           | In PreTerm PreTerm -- t :: S
           | SApp PreTerm PreTerm -- Vec U
           | TApp PreTerm PreTerm -- (Vec U) n
+            -- prog
           | App PreTerm PreTerm -- t n
           | Lambda VName PreTerm -- \ x. t
+            -- proof term
+          | MP PreTerm PreTerm -- mp p1 by p2
+          | Inst PreTerm PreTerm -- inst p1 by p2
+          | UG VName PreTerm     -- ug x . p
+          | Cmp PreTerm          -- cmp p
+          | InvCmp PreTerm PreTerm  -- invcmp p from F
+          | Beta PreTerm            -- beta p
+          | InvBeta PreTerm PreTerm  -- invbeta p from F
+          | Discharge VName (Maybe PreTerm) PreTerm -- discharge a : F . p
+          | PLam VName PreTerm      -- \ x . p 
+          | PApp PreTerm PreTerm     -- p1 t
+          | PFApp PreTerm PreTerm    -- p1 p2
           | Pos SourcePos PreTerm
           deriving (Show)
 
@@ -55,260 +68,65 @@ data PNameless = PV Int
              | TAP PNameless PNameless
              | AP PNameless PNameless
              | LM PNameless
+             | MODP PNameless PNameless -- mp p1 by p2
+             | INST PNameless PNameless -- inst p1 by p2
+             | UNGN PNameless     -- ug x . p
+             | CMP PNameless          -- cmp p
+             | INVCMP PNameless PNameless  -- invcmp p from F
+             | BETA PNameless            -- beta p
+             | INVB PNameless PNameless  -- invbeta p from F
+             | DIS (Maybe PNameless) PNameless -- discharge a : F . p
+             | PL VName PNameless      -- \ x . p 
+             | PA PNameless PNameless     -- p1 t
+             | PFA PNameless PNameless    -- p1 p2
              deriving (Show, Eq)
 
-data Proof = Assume VName
-           | PrVar VName
-           | MP Proof Proof 
-           | Inst Proof PreTerm 
-           | UG VName Proof 
-           | Cmp Proof 
-           | InvCmp Proof PreTerm
-           | Beta Proof 
-           | InvBeta Proof PreTerm 
-           | Discharge VName (Maybe PreTerm) Proof
-           | PLam VName Proof
-           | PApp Proof Proof
-           | PFApp Proof PreTerm
-           | PPos SourcePos Proof
-           deriving (Show, Eq)
 
--- get all the free vars
-fPrVar :: Proof -> S.Set VName
-fPrVar (PPos a p) = fPrVar p
-fPrVar (Assume x) = S.empty
-fPrVar (PrVar x) = S.insert x S.empty
-fPrVar (MP p1 p2) = fPrVar p1 `S.union` fPrVar p2
-fPrVar (PApp p1 p2) = fPrVar p1 `S.union` fPrVar p2
-fPrVar (Inst p1 t) = fPrVar p1 `S.union` fv t
-fPrVar (UG a p) = S.delete a (fPrVar p)
-fPrVar (PLam a p) = S.delete a (fPrVar p)
-fPrVar (Discharge a t p) =
-  case t of
-    Nothing -> S.delete a (fPrVar p)
-    Just b -> S.delete a (fPrVar p) `S.union` fv b
-fPrVar (Cmp p) = fPrVar p
-fPrVar (Beta p) = fPrVar p
-fPrVar (InvCmp p1 t) = fPrVar p1 `S.union` fv t
-fPrVar (InvBeta p1 t) = fPrVar p1 `S.union` fv t
-fPrVar (PFApp p1 t) = fPrVar p1 `S.union` fv t
--- naive sub proof 
-naiveSub :: Proof -> Proof -> Proof -> Proof
-naiveSub p (PrVar x) (PrVar y) =
-  if x == y then p else PrVar y
+
+-- naive sub for proof 
+naiveSub :: PreTerm -> PreTerm -> PreTerm -> PreTerm
+naiveSub p (PVar x) (PVar y) =
+  if x == y then p else PVar y
                                
-naiveSub p (PrVar x) (MP p1 p2) = 
-  let a1 = naiveSub p (PrVar x) p1
-      a2 = naiveSub p (PrVar x) p2 in
+naiveSub p (PVar x) (MP p1 p2) = 
+  let a1 = naiveSub p (PVar x) p1
+      a2 = naiveSub p (PVar x) p2 in
   MP a1 a2
   
-naiveSub p (PrVar x) (Inst p1 t) =
-  let a = naiveSub p (PrVar x) p1 in
+naiveSub p (PVar x) (Inst p1 t) =
+  let a = naiveSub p (PVar x) p1 in
   Inst a t
   
-naiveSub p (PrVar x) (UG y p1) = UG y (naiveSub p (PrVar x) p1)
+naiveSub p (PVar x) (UG y p1) = UG y (naiveSub p (PVar x) p1)
                                      
-naiveSub p (PrVar x) (Cmp p1) = Cmp (naiveSub p (PrVar x) p1)
+naiveSub p (PVar x) (Cmp p1) = Cmp (naiveSub p (PVar x) p1)
                                      
-naiveSub p (PrVar x) (InvCmp p1 t) =
-  InvCmp (naiveSub p (PrVar x) p1) t
+naiveSub p (PVar x) (InvCmp p1 t) =
+  InvCmp (naiveSub p (PVar x) p1) t
 
-naiveSub p (PrVar x) (Beta p1) =
-  Beta (naiveSub p (PrVar x) p1)
+naiveSub p (PVar x) (Beta p1) =
+  Beta (naiveSub p (PVar x) p1)
                                     
-naiveSub p (PrVar x) (InvBeta p1 t) =
-  InvBeta ( naiveSub p (PrVar x) p1) t
+naiveSub p (PVar x) (InvBeta p1 t) =
+  InvBeta ( naiveSub p (PVar x) p1) t
 
-naiveSub p (PrVar x) (Discharge y t p1) = Discharge y t (naiveSub p (PrVar x) p1)
+naiveSub p (PVar x) (Discharge y t p1) = Discharge y t (naiveSub p (PVar x) p1)
   
-naiveSub p (PrVar x) (PLam y p1) =  PLam y (naiveSub p (PrVar x) p1)
+naiveSub p (PVar x) (PLam y p1) =  PLam y (naiveSub p (PVar x) p1)
   
-naiveSub p (PrVar x) (PApp p1 p2) = 
-  let a1 = naiveSub p (PrVar x) p1
-      a2 = naiveSub p (PrVar x) p2 in
+naiveSub p (PVar x) (PApp p1 p2) = 
+  let a1 = naiveSub p (PVar x) p1
+      a2 = naiveSub p (PVar x) p2 in
   PApp a1 a2
   
-naiveSub p (PrVar x) (PFApp p1 t) = 
-  PFApp (naiveSub p (PrVar x) p1) t
+naiveSub p (PVar x) (PFApp p1 t) = 
+  PFApp (naiveSub p (PVar x) p1) t
   
-naiveSub p (PrVar x) (PPos a p1) =
-  PPos a (naiveSub p (PrVar x) p1)
+naiveSub p (PVar x) (Pos a p1) =
+  Pos a (naiveSub p (PVar x) p1)
 
--- capture avoiding subst for proof
-runSubProof :: Proof -> Proof -> Proof -> Proof
-runSubProof t x t1 = fst $ runState (subProof t x t1) 0
-
-runSubPre :: PreTerm -> PreTerm -> Proof -> Proof
-runSubPre t x t1 = fst $ runState (subPre t x t1) 0
-
-subProof :: Proof -> Proof -> Proof -> GVar Proof
-subProof p (PrVar x) (PrVar y) =
-  if x == y then return p else return $ PrVar y
-                               
-subProof p (PrVar x) (MP p1 p2) = do
-  a1 <- subProof p (PrVar x) p1
-  a2 <- subProof p (PrVar x) p2
-  return $ MP a1 a2
-  
-subProof p (PrVar x) (Inst p1 t) =
-  subProof p (PrVar x) p1 >>= \ a -> return $ Inst a t
-  
-subProof p (PrVar x) (UG y p1) =
-  if x == y || not (x `S.member` fPrVar p1) then return $ UG y p1
-  else if not (y `S.member` fPrVar p)
-       then do
-         c <- subProof p (PrVar x) p1
-         return $ UG y c
-       else do
-         n <- get
-         modify (+1)
-         c1 <- subPre (PVar (y++ show n)) (PVar y) p1
-         c2 <- subProof p (PrVar x) c1
-         return $ UG (y++ show n) c2
-                                     
-subProof p (PrVar x) (Cmp p1) =
-  subProof p (PrVar x) p1 >>= \ a -> return $ Cmp a
-                                     
-subProof p (PrVar x) (InvCmp p1 t) =
-  subProof p (PrVar x) p1 >>= \ a -> return $ InvCmp a t
-
-subProof p (PrVar x) (Beta p1) =
-  subProof p (PrVar x) p1 >>= \a -> return $ Beta a
-                                    
-subProof p (PrVar x) (InvBeta p1 t) =
-  subProof p (PrVar x) p1 >>= \ a -> return $ InvBeta a t
-
-subProof p (PrVar x) (Discharge y t p1) = 
-  if x == y || not (x `S.member` fPrVar p1) then return $ Discharge y t p1
-  else if not (y `S.member` fPrVar p)
-       then do
-         c <- subProof p (PrVar x) p1
-         return $ Discharge y t c
-       else do
-         n <- get
-         modify (+1)
-         c1 <- subProof (PrVar (y++ show n)) (PrVar y) p1
-         c2 <- subProof p (PrVar x) c1
-         return $ Discharge (y++ show n) t c2
-
-subProof p (PrVar x) (PLam y p1) =
-  if x == y || not (x `S.member` fPrVar p1) then return $ PLam y p1
-  else if not (y `S.member` fPrVar p)
-       then do
-         c <- subProof p (PrVar x) p1
-         return $ PLam y c
-       else do
-         n <- get
-         modify (+1)
-         c1 <- subProof (PrVar (y++ show n)) (PrVar y) p1
-         c2 <- subProof p (PrVar x) c1
-         return $ PLam (y++ show n) c2
-
-subProof p (PrVar x) (PApp p1 p2) = do
-  a1 <- subProof p (PrVar x) p1
-  a2 <- subProof p (PrVar x) p2
-  return $ PApp a1 a2
-  
-subProof p (PrVar x) (PFApp p1 t) = 
-  subProof p (PrVar x) p1 >>= \ a -> return $ PFApp a t
-  
-subProof p (PrVar x) (PPos a p1) =
-  subProof p (PrVar x) p1 >>= \ b -> return $ PPos a b
-
-subPre :: PreTerm -> PreTerm -> Proof -> GVar Proof
-subPre p (PVar x) (PrVar y) = return $ PrVar y
-subPre p (PVar x) (MP p1 p2) = do
-  a1 <- subPre p (PVar x) p1
-  a2 <- subPre p (PVar x) p2
-  return $ MP a1 a2
-  
-subPre p (PVar x) (Inst p1 t) = do
-  a1 <- subPre p (PVar x) p1
-  t1 <- subst p (PVar x) t
-  return $ Inst a1 t1
-  
-subPre p (PVar x) (UG y p1) =
-  if x == y || not (x `S.member` fPrVar p1) then return $ UG y p1
-  else if not (y `S.member` fv p)
-       then do
-         c <- subPre p (PVar x) p1
-         return $ UG y c
-       else do
-         n <- get
-         modify (+1)
-         c1 <- subPre (PVar (y++ show n)) (PVar y) p1
-         c2 <- subPre p (PVar x) c1
-         return $ UG (y++ show n) c2
-
-subPre p (PVar x) (PLam y p1) =
-  if x == y || not (x `S.member` fPrVar p1) then return $ PLam y p1
-  else if not (y `S.member` fv p)
-       then do
-         c <- subPre p (PVar x) p1
-         return $ PLam y c
-       else do
-         n <- get
-         modify (+1)
-         c1 <- subProof (PrVar (y++ show n)) (PrVar y) p1
-         c3 <- subPre (PVar (y++ show n)) (PVar y) c1
-         c2 <- subPre p (PVar x) c3
-         return $ PLam (y++ show n) c2
-
-subPre p (PVar x) (Discharge y Nothing p1) = 
-  if x == y || not (x `S.member` fPrVar p1) then return $ Discharge y Nothing p1
-  else if not (y `S.member` fv p)
-       then do
-         c <- subPre p (PVar x) p1
-         return $ Discharge y Nothing c
-       else do
-         n <- get
-         modify (+1)
-         c1 <- subProof (PrVar (y++ show n)) (PrVar y) p1
-         c2 <- subPre p (PVar x) c1
-         return $ Discharge (y++ show n) Nothing c2
-
-subPre p (PVar x) (Discharge y (Just t) p1) = 
-  subst p (PVar x) t >>= \ t1 ->
-  if x == y || not (x `S.member` fPrVar p1) then return $ Discharge y (Just t1) p1
-  else if not (y `S.member` fv p)
-       then do
-         c <- subPre p (PVar x) p1
-         return $ Discharge y (Just t1) c
-       else do
-         n <- get
-         modify (+1)
-         c1 <- subProof (PrVar (y++ show n)) (PrVar y) p1
-         c2 <- subPre p (PVar x) c1
-         return $ Discharge (y++ show n) (Just t1) c2
-  
-subPre p (PVar x) (Cmp p1) = 
-  subPre p (PVar x) p1 >>= \ a -> return $ Cmp a
-
-subPre p (PVar x) (InvCmp p1 t) = do
-  a1 <- subPre p (PVar x) p1
-  t1 <- subst p (PVar x) t
-  return $ InvCmp a1 t1
-  
-subPre p (PVar x) (Beta p1) = subPre p (PVar x) p1 >>= \ a -> return $ Beta a
-subPre p (PVar x) (InvBeta p1 t) = do
-  a <- subPre p (PVar x) p1
-  t1 <- subst p (PVar x) t
-  return $ InvBeta a t1
-  
-subPre p (PVar x) (PApp p1 p2) = do
-  a1 <- subPre p (PVar x) p1
-  a2 <- subPre p (PVar x) p2
-  return $ PApp a1 a2
-  
-subPre p (PVar x) (PFApp p1 t) = do
-  a1 <- subPre p (PVar x) p1
-  t1 <- subst p (PVar x) t
-  return $ PFApp a1 t1
-
-subPre p (PVar x) (PPos a p1) = subPre p (PVar x) p1 >>= \ b -> return $ PPos a b
-
-  
-type ProofScripts = [(VName, Proof, Maybe PreTerm)]
+data Assumption = Assume VName deriving Show
+type ProofScripts = [(VName, Either Assumption PreTerm, Maybe PreTerm)]
 
 data Prog = Name VName 
           | Applica Prog Prog
@@ -344,7 +162,7 @@ data Decl = ProgDecl VName Prog
           | ProofDecl VName (Maybe VName) ProofScripts PreTerm
           | DataDecl SourcePos Datatype
           | SetDecl VName PreTerm
-          | TacDecl VName [VName] (Either Proof ProofScripts)
+          | TacDecl VName [VName] (Either PreTerm ProofScripts)
           | FormOperatorDecl String Int String
           | ProgOperatorDecl String Int String
           deriving Show
@@ -360,25 +178,28 @@ fv (Lambda x s) = S.delete x (fv s)
 fv (App f1 f2) = fv f1 `S.union` fv f2
 fv (SApp f1 f2) = fv f1 `S.union` fv f2
 fv (TApp f1 f2) = fv f1 `S.union` fv f2
+fv (MP p1 p2) = fv p1 `S.union` fv p2
+fv (PApp p1 p2) = fv p1 `S.union` fv p2
+fv (Inst p1 t) = fv p1 `S.union` fv t
+fv (UG a p) = S.delete a (fv p)
+fv (PLam a p) = S.delete a (fv p)
+fv (Discharge a t p) =
+  case t of
+    Nothing -> S.delete a (fv p)
+    Just b -> S.delete a (fv p) `S.union` fv b
+fv (Cmp p) = fv p
+fv (Beta p) = fv p
+fv (InvCmp p1 t) = fv p1 `S.union` fv t
+fv (InvBeta p1 t) = fv p1 `S.union` fv t
+fv (PFApp p1 t) = fv p1 `S.union` fv t
 fv (Pos _ p) = fv p
-
--- get the free set variables
-fVar :: PreTerm -> S.Set VName
-fVar (PVar x) = S.insert x S.empty
-fVar (Imply f1 f2) = fVar f1 `S.union` fVar f2
-fVar (Forall x f) = S.delete x (fVar f)
-fVar (In t s) = fVar s
-fVar (Iota x s) = S.delete x (fVar s)
-fVar (Lambda x s) = S.empty
-fVar (App f1 f2) = S.empty
-fVar (SApp f1 f2) = fVar f1 `S.union` fVar f2
-fVar (TApp f1 f2) = fVar f1
-fVar (Pos _ f) = fVar f 
 
 type GVar a = State Int a
 
 type BindCxt a = Reader [(VName, Int)] a
 
+-- only for prog term and formula term comparison
+-- we don't want to compare to proof term at all
 debruijn :: PreTerm -> BindCxt PNameless
 debruijn (PVar x) = do 
   Just n <- asks (lookup x) 
@@ -512,6 +333,95 @@ subst s (PVar x) (Lambda a f) =
          c1 <- subst (PVar (a++ show n)) (PVar a) f
          c2 <- subst s (PVar x) c1
          return $ Lambda (a++ show n) c2
+
+subst p (PVar x) (MP p1 p2) = do
+  a1 <- subst p (PVar x) p1
+  a2 <- subst p (PVar x) p2
+  return $ MP a1 a2
+  
+subst p (PVar x) (Inst p1 t) = do
+  a <- subst p (PVar x) p1
+  a1 <- subst p (PVar x) t
+  return $ Inst a a1
+  
+subst p (PVar x) (UG y p1) =
+  if x == y || not (x `S.member` fv p1) then return $ UG y p1
+  else if not (y `S.member` fv p)
+       then do
+         c <- subst p (PVar x) p1
+         return $ UG y c
+       else do
+         n <- get
+         modify (+1)
+         c1 <- subst (PVar (y++ show n)) (PVar y) p1
+         c2 <- subst p (PVar x) c1
+         return $ UG (y++ show n) c2
+                                     
+subst p (PVar x) (Cmp p1) =
+  subst p (PVar x) p1 >>= \ a -> return $ Cmp a
+                                     
+subst p (PVar x) (InvCmp p1 t) = do
+  a <- subst p (PVar x) p1
+  a1 <- subst p (PVar x) t
+  return $ InvCmp a a1
+
+subst p (PVar x) (Beta p1) =
+  subst p (PVar x) p1 >>= \a -> return $ Beta a
+                                    
+subst p (PVar x) (InvBeta p1 t) = do
+  a <- subst p (PVar x) p1
+  a1 <- subst p (PVar x) t
+  return $ InvBeta a a1
+
+subst p (PVar x) (Discharge y (Just t) p1) = do
+  t1 <- subst p (PVar x) t
+  if x == y || not (x `S.member` fv p1) then return $ Discharge y (Just t1) p1
+  else if not (y `S.member` fv p)
+       then do
+         c <- subst p (PVar x) p1
+         return $ Discharge y (Just t1) c
+       else do
+         n <- get
+         modify (+1)
+         c1 <- subst (PVar (y++ show n)) (PVar y) p1
+         c2 <- subst p (PVar x) c1
+         return $ Discharge (y++ show n) (Just t1) c2
+
+subst p (PVar x) (Discharge y Nothing p1) = 
+  if x == y || not (x `S.member` fv p1) then return $ Discharge y (Nothing) p1
+  else if not (y `S.member` fv p)
+       then do
+         c <- subst p (PVar x) p1
+         return $ Discharge y (Nothing) c
+       else do
+         n <- get
+         modify (+1)
+         c1 <- subst (PVar (y++ show n)) (PVar y) p1
+         c2 <- subst p (PVar x) c1
+         return $ Discharge (y++ show n) (Nothing) c2
+
+subst p (PVar x) (PLam y p1) =
+  if x == y || not (x `S.member` fv p1) then return $ PLam y p1
+  else if not (y `S.member` fv p)
+       then do
+         c <- subst p (PVar x) p1
+         return $ PLam y c
+       else do
+         n <- get
+         modify (+1)
+         c1 <- subst (PVar (y++ show n)) (PVar y) p1
+         c2 <- subst p (PVar x) c1
+         return $ PLam (y++ show n) c2
+
+subst p (PVar x) (PApp p1 p2) = do
+  a1 <- subst p (PVar x) p1
+  a2 <- subst p (PVar x) p2
+  return $ PApp a1 a2
+  
+subst p (PVar x) (PFApp p1 t) = do
+  a <- subst p (PVar x) p1
+  a1 <- subst p (PVar x) t
+  return $ PFApp a a1
 
 subst s (PVar x) (Pos _ f) = subst s (PVar x) f
 
